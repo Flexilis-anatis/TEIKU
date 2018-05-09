b5b3523e3d6e205908c5e9a2215cff6c2dececd5
RubberDuckyMaster <ihateemail@irony.com>
2018-05-08 23:12:37 -0700

    Added gui.h as collection of gui files. Extended main.cpp doc comment

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
-#include "source/gui/image.h"
+#include "source/gui.h"
 
 /**
 Author:     RubberDuckyMaster
 Start date: 5/8/2018, 11:03 AM
 Name:       TextEditor (I know, Uninventive) (TEIKU for short ;)
-Purpose:    Basic, notepad like text editor.
+Purpose:    Basic text editor.
 
-Just a little program I'm writing to get more practice with GUI and font rendering.
+Just a little program I'm writing to get more practice with GUI's
+and text rendering. Hoping to - in the end - at least get it up
+to the level of notepad, as simple a goal as that may sound.
+
+I'm going to try and design my own file selector for
+saving/opening, because I hate the Win32 API.
 */
 
 int main()
---
+++ source/gui.h
@@ -0,0 +1,10 @@
+#ifndef GUI_H_INCLUDED
+#define GUI_H_INCLUDED
+
+// A collection of everything in the GUI folder
+
+#include "gui/base/item.h"
+#include "gui/image.h"
+#include "gui/text.h"
+
+#endif // GUI_H_INCLUDED

ba2db6564d57555032a5ed9f0fd22cac2809dbfb
Flexilis-anatis <Flexilisanatis@protonmail.com>
2018-07-23 20:06:52 -0600

    added make script

---
+++ make.sh
@@ -0,0 +1,6 @@
+# Gets all files that end in .h and .cpp
+files="$(find . -type f | grep -v '\./\.git' | grep -v "\.gch" | grep -E '\.cpp|\.h|\.hpp|\.cxx|\.cc|\.hh|\.hxx' | sort -r | sed -n -e 'H;${x;s/\n/ /g;s/^,//;p;}')"
+
+sfml_loc=/usr/include
+
+g++ $files -Werror -Wall -o teiku.out -L$sfml_loc -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system
